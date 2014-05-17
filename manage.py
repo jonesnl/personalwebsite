e989402fa674a76c04ac180583bfc5fb317fb0c3
Nate Jones <jonesnl@umich.edu>
2014-05-17 22:58:50 +0000

    if on server use different settings file

--- manage.py
+++ manage.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python2
 import os
 import sys
+import socket
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personalsite.settings.dev")
-
+    if socket.gethostname() == 'PersonalSite':
+    	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personalsite.settings.prod")
+    else: 
+    	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personalsite.settings.dev")
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
