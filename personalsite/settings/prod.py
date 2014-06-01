ae1886cc83aa53e27ea345b3b14b73ceb2cdfe92
Nate Jones <jonesnl@umich.edu>
2014-06-01 14:29:24 -0400

    Change database location for production

--- personalsite/settings/prod.py
+++ personalsite/settings/prod.py
@@ -4,3 +4,14 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'www.nathaniel-jones.com', 'nathaniel-jones.com']
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/etc/httpd/db/personalsite.sqlite3',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
