9c129f2c901e259cacdae694dbeb1b13904a31b0
Nate Jones <jonesnl@umich.edu>
2014-06-01 15:37:31 -0400

    Show blog in website

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from blog import views
 urlpatterns = patterns('',
                        url(r'^(?P<post_id>\d+)', views.post),
                        url(r'^latest$', views.latest_post),
-                       url(r'^$', views.latest_post),
+                       url(r'^$', views.latest_post, name='blog'),
 )
