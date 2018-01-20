0b55cb641c7463d960a5bfe84969a181e7b5381d
Nate Jones <jonesnl@umich.edu>
2018-01-20 14:59:20 -0500

    Fix some more errors with newer django version

--- blog/views.py
+++ blog/views.py
@@ -9,15 +9,15 @@ def post(request, post_id):
     #Fill variables from database
     post = Post.objects.get(id=int(post_id))
 
-    context = RequestContext(request, {
+    context = {
         'id': int(post_id),
         'title': post.title,
         'body': post.body,
         'created': post.created,
         'author_first': post.author.first_name,
         'author_last': post.author.last_name,
-    })
-    return HttpResponse(template.render(context))
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def latest_posts(request, page_num=1):
@@ -32,9 +32,6 @@ def latest_posts(request, page_num=1):
     posts = Post.objects.order_by('-created')
     cur_page_posts = posts[(page_num*5):(page_num+1)*5]
 
-    if len(cur_page_posts) <= 0:
-        raise Http404
-
     if len(posts) > (page_num + 1) * 5:
         older_posts = True
     else:
@@ -45,11 +42,11 @@ def latest_posts(request, page_num=1):
     else:
         newer_posts = False
 
-    context = RequestContext(request, {
+    context = {
         'post_list': cur_page_posts,
         'page_num': page_num+1,
         'newer_posts': newer_posts,
         'older_posts': older_posts,
-    })
+    }
 
-    return HttpResponse(template.render(context))
+    return HttpResponse(template.render(context, request))
