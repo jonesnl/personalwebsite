c3738f36ffcdd785866458a8bf7904045b151e84
Nate Jones <jonesnl@umich.edu>
2018-03-04 10:53:36 -0500

    Add who is pinging to the website

--- ping/views.py
+++ ping/views.py
@@ -8,35 +8,17 @@ from django.urls import reverse
 
 from .models import Ping
 
-# Create your views here.
-
-def login_view(request):
-    """Login user"""
-    username = request.POST['username']
-    password = request.POST['password']
-
-    user = authenticate(request, username=username, password=password)
-
-    if user is not None:
-        login(request, user)
-        print('SUCCESS you logged in!')
-        # redirect to success page...
-        return redirect('test_view')
-    else:
-        # return invalid login msg
-        return HttpResponse('FAILED to login bro')
-
-def logout_view(requst):
-    """Logout user"""
-    logout(request)
-    print('successful logout')
-    # redirect to success page
-
 @login_required(login_url='/ping/accounts/login')
 def test_view(request):
     if request.method == 'GET':
         template = loader.get_template('ping/index.html')
-        return HttpResponse(template.render(request=request))
+        pinging_list = (Ping.objects
+                            .filter(pinging=True)
+                            .exclude(user=request.user))
+        context = {
+            'pinging_list': pinging_list,
+        }
+        return HttpResponse(template.render(request=request, context=context))
     elif request.method == 'POST':
         ping_obj = get_object_or_404(Ping, user=request.user)
         ping_obj.pinging = True
